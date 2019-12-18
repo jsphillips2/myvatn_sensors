0afca7a9600ec6067d9675b4b262717318da630e
Unknown <jsphillips2@wisc.edu>
2019-12-18 14:46:23 -0600

    script for cleaning HOBO data

---
+++ hobo/hobo_clean.R
@@ -0,0 +1,72 @@
+#==========
+#========== Preliminaries
+#==========
+
+# load packages
+library(tidyverse)
+library(lubridate)
+Sys.setenv(TZ = "GMT")
+
+# set cores for parallel
+options(mc.cores = parallel::detectCores()-2)
+
+# define flagging function
+flag_fn <- function(x){x[!is.na(x)] %>% length()}
+
+# import and process data
+hobo_list <- list.files("hobo/raw", full.names = T) %>%
+  parallel::mclapply(function(x){
+    
+    type = substr(x, nchar(x)-2, nchar(x))
+    
+    if(type == "txt") 
+    { data = read_csv(x, skip = 2, col_names = F)
+      sn = {read_csv(x, skip = 1, col_names = F)[1, 3] %>%
+            str_split("SEN ") %>% unlist()}[2] %>% substr(6, nchar(.)-1)
+    } 
+    
+    if(type == "tsv") 
+    { data = read_tsv(x, skip = 2, col_names = F)
+      sn = {read_tsv(x, skip = 1, col_names = F)[1, 3] %>%
+            str_split("SEN ") %>% unlist()}[2] %>% substr(6, nchar(.)-1)
+    }
+    
+    data = data %>%
+      select(2, 3, 4) %>%
+      set_names(c("datetime","temp","lux")) %>%
+      mutate(datetime = 
+               parse_date_time(datetime, orders = 
+                                 c("mdy HMS","mdy HM", "mdY HMS", "mdY HM")),
+             name = str_remove(x, "hobo/raw/"),
+             sn = sn) %>%
+      select(name, sn, datetime, temp, lux)
+    
+    flag = if(min(apply(data, 2, flag_fn))== 0) "yes" else "no"
+    
+    data = data %>%
+      mutate(flag = flag)
+    
+    return(data)
+  })
+
+# check for flags
+hobo_list %>% lapply(function(x){
+  x %>% select(name, flag) %>% unique()
+}) %>%
+  bind_rows() %>% 
+  filter(flag == "yes")
+
+# fix file with missing temp
+if(hobo_list[[32]]$flag[1] == "yes") {
+  hobo_list[[32]] <- hobo_list[[32]] %>%
+    select(-lux) %>%
+    rename(lux = temp) %>%
+    mutate(flag = "no")
+}
+
+# bind rows
+hobos = hobo_list %>%
+  bind_rows() %>%
+  arrange(datetime, name)
+
+
